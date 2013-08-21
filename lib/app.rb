c8cb6cc7f10b8d4a0b00894e815ef3ac6fc03a06
baya <kayak.jiang@gmail.com>
2013-08-22 00:20:06 +0800

    remove app

--- lib/app.rb
+++
@@ -1,7 +0,0 @@
-module Description
-  App = Ground 'description' do
-    use Rack::ShowExceptions
-    use Rack::Static, urls: ['/assets']
-  end
-end
-
--- lib/description.rb
+++ lib/description.rb
@@ -2,10 +2,18 @@ require 'bundler/setup'
 Bundler.require(:default)
 
 require 'pp'
-require 'app'
 require 'activities/parse_create_table'
 require 'activities/create_markdown_table'
 require 'activities/describle_table'
 require 'activities/parse_command_options'
 require 'activities/states/tables'
 require 'config/routes'
+
+
+module Description
+  App = Ground 'description' do
+    use Rack::ShowExceptions
+    use Rack::Static, urls: ['/assets']
+  end
+end
+
