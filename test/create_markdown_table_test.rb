c8471f09c5898136d4f8852829136f9884179ed8
jiangguimin <jiangguimin@funguide.com.cn>
2013-08-21 11:16:41 +0800

    fix test

--- test/create_markdown_table_test.rb
+++ test/create_markdown_table_test.rb
@@ -3,15 +3,9 @@ require 'test_helper'
 
 class CreateMarkdownTableTest < Test::Unit::TestCase
 
-  def test_create_markdown_table_header
-    table = CreateMarkdownTalbe header: ['Column', 'Type', 'Modifiers', 'Comment'], rows: []
-    assert_equal table, <<-EOF.strip
-      Column | Type | Modifiers | Comment
-    EOF
-  end
-
-  def test_create_markdown_table_header
+  def test_create_markdown_table
     table = CreateMarkdownTalbe header: ['Column', 'Type', 'Modifiers', 'Comment'], rows: [['id', 'integer', '', '']]
+    assert_equal table, "Column | Type    | Modifiers | Comment\n------ | ------- | --------- | -------\nid     | integer |           |        "
   end
 
 end
