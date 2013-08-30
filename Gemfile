84042b73b5d40841087b6c5200ad3fb70d6e9738
baya <kayak.jiang@gmail.com>
2013-08-30 23:16:22 +0800

    Gemfile

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# source 'https://rubygems.org'
+source 'http://ruby.taobao.org'
+
+gem 'dun'
+gem 'ground', git: 'git://github.com/baya/ground.git', ref: 'bd4f89f0073c'
+gem 'sexp_path'
+gem 'ruby_parser'
+gem 'json'
--- lib/description.rb
+++ lib/description.rb
@@ -1,13 +1,9 @@
 # ground_dir = '/Users/jiangguimin/Projects/ground/lib'
-ground_dir = '/Users/jim/Projects/ground/lib'
-$LOAD_PATH.unshift ground_dir unless $LOAD_PATH.include?(ground_dir)
+# ground_dir = '/Users/jim/Projects/ground/lib'
+# $LOAD_PATH.unshift ground_dir unless $LOAD_PATH.include?(ground_dir)
 
-require 'dun'
-require 'ground'
-require 'sexp_path'
-require 'ruby_parser'
-require 'kramdown'
-require 'json'
+require 'bundler'
+Bundler.require(:default)
 require 'pp'
 require 'optparse'
 require 'logger'
